56ba6cb822ba5b3b1d96c0dc419586838ce0101c
James DeVore <james@measuringu.com>
2025-05-22 13:56:46 -0600

    trying github fix

--- home/default.nix
+++ home/default.nix
@@ -153,7 +153,7 @@
       init.defaultBranch = "main";
       pull.rebase = true;
       # Prefer SSH for GitHub URLs
-      "url.git@github.com:.insteadOf" = "https://github.com/";
+      "url \"git@github.com:\".insteadOf" = "https://github.com/";
     };
   };
 
